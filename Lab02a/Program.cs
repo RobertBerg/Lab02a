434bdf565e9e8620fdcba3bdac49a5250020b7cd
RobertBerg <54758314+RobertBerg@users.noreply.github.com>
2020-03-30 15:18:28 -0400

    Add project files.

---
+++ Lab02a/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Lab02a
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int number1; // declare first number to add
+            int number2; // declare second number to add
+            int sum; // declare sum of number1 and number2
+            
+                Console.Write("Enter first integer: "); // prompt user
+                // read first number from user
+                number1 = Convert.ToInt32(Console.ReadLine());
+
+                Console.Write("Enter Second integer: "); // prompt user
+                // read second number from user
+                number2 = Convert.ToInt32(Console.ReadLine());
+
+                sum = number1 + number2; // add numbers
+
+                Console.WriteLine("Sum is {0}", sum); // display sum
+            } // end Main
+        } // end class Program
+
+    }
+
+
+
+
+
+    
